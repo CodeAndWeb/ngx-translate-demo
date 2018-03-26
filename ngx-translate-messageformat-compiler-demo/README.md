62f52f4c4419fcb39dae5e0e7edcd0d76a7e0a59
Andreas Loew <andreas@codeandweb.com>
2018-03-26 20:22:30 +0200

    Added demo for ngx-translate-messageformat-compiler

---
+++ ngx-translate-messageformat-compiler-demo/README.md
@@ -0,0 +1,7 @@
+# ngx-translate-demo
+
+Simple demo project for ngx-translate 
+
+Read the tutorial [How to translate your Angular app with ngx-translate](https://www.codeandweb.com/babeledit/tutorials/how-to-translate-your-angular-app-with-ngx-translate) for more information about how to use ngx-translate, ngx-translate-extrace and BabelEdit.
+
+
---
+++ ngx-translate-messageformat-compiler-demo/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+
+// import ngx-translate and the http loader
+import {TranslateCompiler, TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+import { PruningTranslationLoader } from './pruning-loader';
+import {TranslateMessageFormatCompiler} from "ngx-translate-messageformat-compiler";
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            },
+            compiler: {
+                provide: TranslateCompiler,
+                useClass: TranslateMessageFormatCompiler
+            }
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+    return new PruningTranslationLoader(http);
+//    return new TranslateHttpLoader(http);
+}
