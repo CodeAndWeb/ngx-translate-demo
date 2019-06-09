c62afdd4cbec3ec4ef211dd49024431bbc3a7106
Andreas Loew <andreas@codeandweb.com>
2019-06-09 11:48:15 +0200

    Added static loading of primary language

--- ngx-translate-demo/src/app/app.component.ts
+++ ngx-translate-demo/src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {_} from '@biesbjerg/ngx-translate-extract/dist/utils/utils';
 
+import defaultLanguage from "./../assets/i18n/en.json";
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -12,6 +14,7 @@ export class AppComponent {
     fromCode = _('demo.text-in-code');
 
     constructor(private translate: TranslateService) {
+        translate.setTranslation('en', defaultLanguage);
         translate.setDefaultLang('en');
     }
 
